423bcce49631be107f05da885eac847b918c59e7
AkashKeote <keoteakash@gmail.com>
2025-08-02 22:14:29 +0530

    Update web build with correct base href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "194744467"
+    serviceWorkerVersion: "967016812"
   }
 });
